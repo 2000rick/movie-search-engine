c60708f7362e1bf257b44ed1615759d194be1750
siaech <chard007@ucr.edu>
2021-02-24 17:51:29 -0800

    Added a main

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include "../header/movies.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  Movies movies;
+
+  string sort;
+  cout << "Choose a sorting method (trending, now_playing, or top_rated)" << endl;
+  cin >> sort;
+
+  int num_movies;
+  cout << "How many movies should we grab?" << endl;
+  cin >> num_movies;
+
+  if (movies.movie_update(sort, num_movies)) {
+    cout << "Movies fetched!" << endl;
+  } else {
+    cout << "Something went wrong!" << endl;
+    return 1;
+  }
+
+  string search;
+  cout << "What do you want to search for? Example: genres=Action AND NOT title=Hard OR production_country=States" << endl;
+  cout << "Possible search inputs: budget, genres, homepage, id, imdb_id, original_language, original_title, overview, popularity, production_company_name, production_company_country, production_country, release_date, revenue, runtime, spoken_language, status, tagline, title, vote_average, vote_count" << endl;
+  getline(cin, search);
+  if (movies.search(search)) {
+    cout << "Movies searched!" << endl;
+  } else {
+    cout << "Something went wrong!" << endl;
+    return 1;
+  }
+
+  movies.print_selection(cout);
+}
