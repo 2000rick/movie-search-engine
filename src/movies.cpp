ea624cac99f3727890613c4cd051b890862886d7
Rick <qipeng.liang.edu@gmail.com>
2021-03-05 21:59:51 -0800

    valid check function now considers lowercase operators like 'and'

--- src/movies.cpp
+++ src/movies.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <memory>
 #include <sstream>
+#include <set>
 
 #include <jsoncpp/json/json.h>
 #include <curl/curl.h>
@@ -139,23 +140,27 @@ Select* Movies::search_helper(std::stringstream& stream) {
 }
 
 bool Movies::valid(std::string &query) {
+    std::set<std::string> logicOps = { "NOT", "not", "AND", "and", "OR", "or"};
     //All queries must have an '='
     if(query.find('=') == string::npos) return false;
     //Query cannot begin with a binary operator (AND or OR)
     std::stringstream s(query);
-	std::string word = "";
+	  std::string word = "";
     s >> word;
-    if(word == "AND" || word == "OR") return false;
+    if(word == "AND" || word == "and" || word == "OR" || word == "or") return false;
     //If an element in the query is not an operator, then it must contain '='
-    if(word != "NOT" && word.find('=') == string::npos) return false;
+    if(word != "NOT" && word != "not" && word.find('=') == string::npos) return false;
     while(s >> word)
     {
-        if(word != "NOT" && word != "AND" && word != "OR") {
+        //if(word != "NOT" && word != "not" && word != "AND" && word != "and" && word != "OR" && word != "or") 
+        if(logicOps.find(word) == logicOps.end()) {
             if(word.find('=') == string::npos) return false;
         }
     }
     //Query cannot end with an operator
-    if(word == "NOT" || word == "AND" || word == "OR") return false;
+    //if(word == "NOT" || word == "AND" || word == "OR" || word == "or" || word == "and" || word == "not") return false;
+    if(logicOps.find(word) != logicOps.end()) return false;
+
     /*NO binary operator should be immedidately followed by another binary operator
     e.g. Genre=Action AND OR Actor=Elizabeth IS INVALID
     Operator NOT cannot be followed by another operator
@@ -164,13 +169,13 @@ bool Movies::valid(std::string &query) {
     std::stringstream s2(query);
     while(s2 >> word)
     {
-        if(word == "AND" || word == "OR" || word == "NOT") {
-            if(word == "NOT") {
+        if(logicOps.find(word) != logicOps.end()) {
+            if(word == "NOT" || word == "not") {
                 s2 >> word;
-                if(word == "AND" || word == "OR" || word == "NOT") return false;
+                if( logicOps.find(word) != logicOps.end() ) return false;
             }
             s2 >> word;
-            if(word == "AND" || word == "OR" ) return false;
+            if(word == "AND" || word == "and" || word == "OR" || word == "or" ) return false;
         }
     }
 
